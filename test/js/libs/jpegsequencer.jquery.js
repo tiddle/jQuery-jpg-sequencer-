ad6be7f43b9317c9f76879570150afcb3d122b91
Carlo <me@carlocruz.com>
2012-01-08 23:28:26 +1100

    Down direction programmed - Initial direction controls inputted (needs cleaning up)

--- test/js/libs/jpegsequencer.jquery.js
+++ test/js/libs/jpegsequencer.jquery.js
@@ -2,9 +2,9 @@
     
     var pluginName = 'jpeg Sequencer',
         defaults = {
-            direction: "right",
+            direction: "down",
             imageAmount: 3,
-            delay: 100, // milliseconds
+            delay: 200, // milliseconds
             directionWidth: 20, // width of the directional arrows
             directionName: 'jpegseq', // class name of directional
             css: true, //include css stylings for directional arrows
@@ -16,7 +16,7 @@
         this.options = $.extend( {}, defaults, options) ;
  
         this._defaults = defaults;
-        this._name = pluginName;
+        this.  _name = pluginName;
         
         this.init();
     }
@@ -32,14 +32,23 @@
 		var wholeHeight = $('#tmp').height(); //get whole width of image
 		$('#tmp').remove();
 		
-		var elementWidth = wholeWidth/this.options.imageAmount; // calculate the element width
-    	
-        $(this.element).width(elementWidth).height(wholeHeight); // set dimensions of element correctly
+		if(this.options.direction == 'right')
+		{
+			var elementWidth = wholeWidth/this.options.imageAmount; // calculate the element width
+	    	var elementHeight = wholeHeight; // calculate the element height
+		}
+		else
+		{
+			var elementWidth = wholeWidth; // calculate the element width
+	    	var elementHeight = wholeHeight/this.options.imageAmount;
+		}
+		    	
+        $(this.element).width(elementWidth).height(elementHeight); // set dimensions of element correctly
         $(this.element).css('position', 'relative');
         $(this.element).html('<div class="'+this.options.directionName+' left"></div><div class="'+this.options.directionName+' right"></div>')
         if(this.options.css === true)
         {
-	        $('.'+this.options.directionName).css('height', wholeHeight).css('width', this.options.directionWidth).css('position', 'absolute').css('top', 0);
+	        $('.'+this.options.directionName).css('height', elementHeight).css('width', this.options.directionWidth).css('position', 'absolute').css('top', 0);
 	        $('.'+this.options.directionName+'.left').css('left', 0); // position left direction
 	        $('.'+this.options.directionName+'.right').css('right', 0); // position right direction
         }
@@ -47,27 +56,42 @@
         // mmm i want to clean this up, not sure how
         
         var amountMoved = 0;
+        var direction = this.options.direction;
+        
+        if(this.options.direction == "right")
+        	var movement = elementWidth;
+        else
+        	var movement = elementHeight;
         
         // On click events
-        $(this.element).children('div').bind('click', {movement: elementWidth, wholeWidth: wholeWidth}, function(e) {
+        $(this.element).children('div').bind('click', {movement: movement, wholeWidth: wholeWidth, wholeHeight: wholeHeight, direction: direction}, function(e) {
 			if($(this).hasClass('left'))
 			{
 				if(amountMoved != 0)
 				{
-					
 					amountMoved += e.data.movement;
-					console.log(amountMoved);
-					$(this).parent().css('background-position', amountMoved+'px 0');
+					
+					if(direction == "right")
+						$(this).parent().css('background-position', amountMoved+'px 0');
+					else
+						$(this).parent().css('background-position', '0 '+ amountMoved+'px');
 				}
 			}
 			
 			if($(this).hasClass('right'))
 			{
-				if(amountMoved != ((wholeWidth-e.data.movement) *-1))
+				if(direction == "right")
+					var directionWidth = wholeWidth;
+				else
+					var directionWidth = wholeHeight;
+				if(amountMoved != ((directionWidth-e.data.movement) *-1))
 				{
 					amountMoved -= e.data.movement;
-					console.log(amountMoved);
-					$(this).parent().css('background-position', amountMoved+'px 0');
+					
+					if(direction == "right")
+						$(this).parent().css('background-position', amountMoved+'px 0');
+					else
+						$(this).parent().css('background-position', '0 '+amountMoved+'px');
 				}
 			}
 			        	
@@ -96,7 +120,6 @@
 			if($(thiselement).hasClass('left'))
 			{
 				$(thiselement).parent().children('.left').click();
-				console.log('left');
 			}
 			
 			if($(thiselement).hasClass('right'))
